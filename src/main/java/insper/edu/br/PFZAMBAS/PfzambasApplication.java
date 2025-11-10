c5f5f3dcfd056a930215e757b0211e2b4759276a
Renanmsc <159029726+Renanmsc@users.noreply.github.com>
2025-11-10 09:52:49 -0300

    Add files via upload

---
+++ src/main/java/insper/edu/br/PFZAMBAS/PfzambasApplication.java
@@ -0,0 +1,13 @@
+package insper.edu.br.PFZAMBAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfzambasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfzambasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/insper/edu/br/PFZAMBAS/PfzambasApplicationTests.java
@@ -0,0 +1,13 @@
+package insper.edu.br.PFZAMBAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfzambasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
